04c0994c6f45b8099ffc1ca018b368edb8c68cfe
Jyaru Hernandez <jyaru1110@gmail.com>
2022-05-25 16:04:42 -0500

    init and manifest

---
+++ manifest.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'App',
+    'summary': """""",
+    'description':"""
+    """,
+    'author' : 'odoo',
+    'category' : 'Training',
+    'version' : '0.1',
+    'depends' : ['sale'],
+    'data':[
+       
+    ],
+    'demo' : [
+        
+    ],
+    'license': 'OPL-1'
+}
