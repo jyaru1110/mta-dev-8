6548faa77ca49c4be5312fa222ae82a24709c6ee
Jyaru Hernandez <jyaru1110@gmail.com>
2022-05-26 18:38:32 -0500

    compute

--- app_mta/models/product_template.py
+++ app_mta/models/product_template.py
@@ -8,17 +8,17 @@ class ProductTemplate(models.Model):
     qty_ordered = fields.Integer(string='# Ordered')
     buffer_size = fields.Integer(string="Buffer Size")
     oc = fields.Integer(string="OC")
-    bp_solicitud = fields.Integer(string="Buffer Penetration Solicitadas",)
-                                #compute='_compute_bp_solicitud')
-    bp_transito = fields.Integer(string="Buffer Penetration en Transito")
-                                #, compute='_compute_bp_transito')
-    bp_disponible = fields.Integer(string="Buffer Penetration Disponible",)
-                               # compute='_compute_bp_disponible')
+    bp_solicitud = fields.Integer(string="Buffer Penetration Solicitadas",
+                                compute='_compute_bp_solicitud')
+    bp_transito = fields.Integer(string="Buffer Penetration en Transito"
+                                , compute='_compute_bp_transito')
+    bp_disponible = fields.Integer(string="Buffer Penetration Disponible",
+                               compute='_compute_bp_disponible')
 
-    #@api.depends('qty_available','buffer_size','virtual_available')
-    #def _compute_bp_transito(self):
-     #   for record in self:
-    #        record.bp_transito = (1-((record.buffer_size-record.qty_available-record.virtual_available)/(record.buffer_size)))*100
-    #def _compute_bp_disponible(self):
-    #    for record in self:
-    #        record.bp_disponible = (1-((record.buffer_size-record.qty_available)/(record.buffer_size)))*100
+    @api.depends('qty_available','buffer_size','virtual_available')
+    def _compute_bp_transito(self):
+       for record in self:
+            record.bp_transito = (1-((record.buffer_size-record.qty_available-record.virtual_available)/(record.buffer_size)))*100
+    def _compute_bp_disponible(self):
+        for record in self:
+            record.bp_disponible = (1-((record.buffer_size-record.qty_available)/(record.buffer_size)))*100
