0398c86347ede5a43453eef3a3e24fd64454c159
Jyaru Hernandez <jyaru1110@gmail.com>
2022-05-26 18:50:51 -0500

    list complete

--- app_mta/models/product_template.py
+++ app_mta/models/product_template.py
@@ -6,8 +6,9 @@ class ProductTemplate(models.Model):
     
     # Add a new column to the product.template model
     qty_ordered = fields.Integer(string='# Ordered')
+    qty_transit = fields.Integer(string='# Transit')
     buffer_size = fields.Integer(string="Buffer Size")
-    oc = fields.Integer(string="OC")
+    oc = fields.Integer(string="Orden de compra")
     bp_solicitud = fields.Integer(string="Buffer Penetration Solicitadas",
                                 compute='_compute_bp_solicitud')
     bp_transito = fields.Integer(string="Buffer Penetration en Transito"
@@ -15,10 +16,13 @@ class ProductTemplate(models.Model):
     bp_disponible = fields.Integer(string="Buffer Penetration Disponible",
                                compute='_compute_bp_disponible')
 
-    @api.depends('qty_available','buffer_size','virtual_available')
+    @api.depends('qty_available','buffer_size','qty_transit')
     def _compute_bp_transito(self):
        for record in self:
-            record.bp_transito = (1-((record.buffer_size-record.qty_available-record.virtual_available)/(record.buffer_size)))*100
+            record.bp_transito = (1-((record.buffer_size-record.qty_available-record.qty_transit)/(record.buffer_size)))*100
+    def _compute_bp_solicitud(self):
+        for record in self:
+            record.bp_solicitud = (1-((record.buffer_size-record.qty_ordered-record.qty_available-record.qty_transit)/(record.buffer_size)))*100
     def _compute_bp_disponible(self):
         for record in self:
             record.bp_disponible = (1-((record.buffer_size-record.qty_available)/(record.buffer_size)))*100
