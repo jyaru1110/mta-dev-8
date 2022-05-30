dd71a427d385da7b8083d05738293bfcaa2a9249
Jyaru Hernandez <jyaru1110@gmail.com>
2022-05-30 12:46:34 -0500

    css test

--- app_mta/models/product_template.py
+++ app_mta/models/product_template.py
@@ -9,20 +9,20 @@ class ProductTemplate(models.Model):
     qty_transit = fields.Integer(string='# Transit')
     buffer_size = fields.Integer(string="Buffer Size",default=10)
     oc = fields.Integer(string="Orden de compra")
-    bp_solicitud = fields.Integer(string="Buffer Penetration Solicitadas",
-                                compute='_compute_bp_solicitud')
-    bp_transito = fields.Integer(string="Buffer Penetration en Transito"
-                                , compute='_compute_bp_transito')
-    bp_disponible = fields.Integer(string="Buffer Penetration Disponible",
-                               compute='_compute_bp_disponible')
+    bp_solicitud = fields.Integer(string="Buffer Penetration Solicitadas",)
+                                #compute='_compute_bp_solicitud')
+    bp_transito = fields.Integer(string="Buffer Penetration en Transito")
+                               # , compute='_compute_bp_transito')
+    bp_disponible = fields.Integer(string="Buffer Penetration Disponible",)
+                               #compute='_compute_bp_disponible')
 
-    @api.depends('qty_available','buffer_size','qty_transit')
-    def _compute_bp_transito(self):
-       for record in self:
-            record.bp_transito = (1-((record.buffer_size-record.qty_available-record.qty_transit)/(record.buffer_size)))*100
-    def _compute_bp_solicitud(self):
-        for record in self:
-            record.bp_solicitud = (1-((record.buffer_size-record.qty_ordered-record.qty_available-record.qty_transit)/(record.buffer_size)))*100
-    def _compute_bp_disponible(self):
-        for record in self:
-            record.bp_disponible = (1-((record.buffer_size-record.qty_available)/(record.buffer_size)))*100
+    #@api.depends('qty_available','buffer_size','qty_transit')
+    #def _compute_bp_transito(self):
+    #   for record in self:
+    #        record.bp_transito = (1-((record.buffer_size-record.qty_available-record.qty_transit)/(record.buffer_size)))*100
+    #def _compute_bp_solicitud(self):
+    #    for record in self:
+    #        record.bp_solicitud = (1-((record.buffer_size-record.qty_ordered-record.qty_available-record.qty_transit)/(record.buffer_size)))*100
+    #def _compute_bp_disponible(self):
+   #     for record in self:
+    #        record.bp_disponible = (1-((record.buffer_size-record.qty_available)/(record.buffer_size)))*100
