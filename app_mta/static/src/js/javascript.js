e2405c66a6669fd433c5e45249cf76ea612222cb
Jyaru Hernandez <jyaru1110@gmail.com>
2022-06-14 18:04:23 -0500

    commit js

--- app_mta/static/src/js/javascript.js
+++ app_mta/static/src/js/javascript.js
@@ -2,15 +2,7 @@
 
 odoo.define('app_mta.javascript', function (require) {
     require('web.dom_ready');
-    var ajax = require('web.ajax');
     var botones = document.getElementsByClassName('condition')
-    //var _onButtonClick = function (e) {
-    //    console.log(e)
-    //    ajax.jsonRpc('get_products', 'call', {}).then(function (data) {
-    //        console.log(data)
-    //    });
-    //}
-    //botones[0].addEventListener('click', _onButtonClick);
     console.log("condition classes:")
     console.log(botones)
 });
