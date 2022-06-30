c1fa0c6f96c24f304aab2de78079f862dac695e6
jyaru1110 <odoo@ampd1.odoo.com>
2022-06-30 04:25:58 +0000

    prueba htmlcol to array

--- app_mta/static/src/js/javascript.js
+++ app_mta/static/src/js/javascript.js
@@ -6,8 +6,16 @@ odoo.define('app_mta.javascript', function (require) {
     console.log(botones)
     console.log("Cargado");
     console.log("antes each");
-    console.log($(".condition"))
-    console.log(botones.length)
+    console.log($(".condition"));
+    console.log(botones.length);
+    console.log("botones_array");
+    var botones_array = Array.prototype.slice.call( botones );
+    console.log(botones_array);
+    console.log(botones_array.length);
+    console.log("botones_array_dos");
+    var botones_array_dos = [...botones];
+    console.log(botones_array_dos);
+    console.log(botones_array_dos.length);
     /*$( ".condition" ).each( function(parametro) { console.log(parametro);} );
     console.log("tras each");*/
 });
