bf2ffe1da553e97fc00b89b1d8165de4a5dc75b4
jyaru1110 <odoo@ampd1.odoo.com>
2022-06-30 04:13:04 +0000

    cambios

--- app_mta/static/src/js/.ipynb_checkpoints/javascript-checkpoint.js
+++ app_mta/static/src/js/.ipynb_checkpoints/javascript-checkpoint.js
@@ -1,16 +1,11 @@
 'use strict'
 
 odoo.define('app_mta.javascript', function (require) {
-    require('web.dom_ready');
     var botones = document.getElementsByClassName('condition')
     console.log("condition classes 2:")
     console.log(botones)
-    
-    $(document).ready(function(){
-        $('.condition').each((boton)=>{
-            console.log(boton)
-        }
-        )
-    })
-    
+    console.log("Cargado");
+    console.log("antes each");
+    $( ".condition" ).each( function(parametro) { console.log(parametro);} );
+    console.log("tras each");
 });
