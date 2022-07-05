4a582b6162499cbf5056342b1b60acac7c3fc43a
jyaru1110 <odoo@ampd1.odoo.com>
2022-07-05 02:53:51 +0000

    inventory instead of sale

--- app_mta/__manifest__.py
+++ app_mta/__manifest__.py
@@ -8,7 +8,7 @@
     'author' : 'odoo',
     'category' : 'Training',
     'version' : '0.1',
-    'depends' : ['sale'],
+    'depends' : ['inventory'],
     'data':[
        'security/mta_security.xml',  
        'security/ir.model.access.csv',
