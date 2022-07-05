f7076b456afeb7ca880a4a403a9fb30d5666c27f
jyaru1110 <odoo@ampd1.odoo.com>
2022-07-05 03:38:31 +0000

    sale again

--- app_mta/__manifest__.py
+++ app_mta/__manifest__.py
@@ -8,7 +8,7 @@
     'author' : 'odoo',
     'category' : 'Training',
     'version' : '0.1',
-    'depends' : [],
+    'depends' : ['sale'],
     'data':[
        'security/mta_security.xml',  
        'security/ir.model.access.csv',
