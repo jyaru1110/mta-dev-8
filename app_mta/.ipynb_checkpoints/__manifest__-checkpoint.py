33fae164c70aca29edd8000a6a2bbbb5fe50b8b4
jyaru1110 <odoo@ampd1.odoo.com>
2022-07-05 02:37:01 +0000

    no widget in manifets

--- app_mta/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ app_mta/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -15,15 +15,15 @@
        'views/mta_app_view.xml',
        'views/product_view.xml'
     ],
-    'assets': {
-        'web.assets_backend': [
-            '/app_mta/static/src/css/progress_bar_widget.css',
-            '/app_mta/static/src/js/progress_bar_widget.js',
-        ],
-        'web.assets_qweb': [
-            '/app_mta/static/src/xml/progress_bar_widget.xml',
-        ],
-    },
+    #'assets': {
+    #    'web.assets_backend': [
+   #         '/app_mta/static/src/css/progress_bar_widget.css',
+   #         '/app_mta/static/src/js/progress_bar_widget.js',
+    #    ],
+     #   'web.assets_qweb': [
+      #      '/app_mta/static/src/xml/progress_bar_widget.xml',
+       # ],
+    #},
     'demo' : [
         
     ],
