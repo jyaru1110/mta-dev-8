64db47b11f4d227941a9d8269b1d86895e322fb8
jyaru1110 <odoo@ampd1.odoo.com>
2022-07-07 01:31:45 +0000

    static added again

---
+++ app_mta/static/js/progress_bar_widget.js
@@ -0,0 +1,23 @@
+/** @odoo-module **/
+var fieldRegistry = require('web.field_registry');
+var FieldChar = require('web.basic_fields').FieldChar;
+
+var ProgressBarWidget = FieldChar.extend({
+    
+    _renderReadonly: function () {
+        /*var self = this;
+        var value = this.value;
+        var max_value = 100;
+        value = value || 0;
+        var widthComplete;
+        if (value <= max_value){
+            widthComplete = parseInt(value/max_value * 100);
+        }
+        else{
+            widthComplete = 100;
+        }*/
+        this.$('.progress_number').text('');
+        //this.$('.progress-bar-inner').css('width', widthComplete + '%');
+    },
+})
+fieldRegistry.add('progress_bar_widget', ProgressBarWidget);
