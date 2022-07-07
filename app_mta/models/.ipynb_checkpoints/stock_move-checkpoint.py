e37eaa082248a4fc77ee3b7905b6523ebd192a27
jyaru1110 <odoo@ampd1.odoo.com>
2022-07-07 00:21:51 +0000

    just override stock_move

--- app_mta/models/.ipynb_checkpoints/stock_move-checkpoint.py
+++ app_mta/models/.ipynb_checkpoints/stock_move-checkpoint.py
@@ -4,10 +4,10 @@ from odoo import models, fields, api
 class StockMove(models.Model):
     _inherit = 'stock.move'
     
-    @api.model
-    def create(self,values):
+    #@api.model
+    #def create(self,values):
         # your logic goes here
         #producto = self.env['mta.producto'].search([('id', '=', self.product_id)])
         #producto.oc = producto.oc + self.product_uom_qty
-        override_create = super(StockMove,self).create(values)
-        return override_create
+        #override_create = super(StockMove,self).create(values)
+        #return override_create
