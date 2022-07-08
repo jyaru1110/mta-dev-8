9999569d264879eae5290b0d497f650aeffa920e
jyaru1110 <odoo@ampd1.odoo.com>
2022-07-08 21:39:54 +0000

    stock move browse method

--- app_mta/models/.ipynb_checkpoints/stock_move-checkpoint.py
+++ app_mta/models/.ipynb_checkpoints/stock_move-checkpoint.py
@@ -8,6 +8,8 @@ class StockMove(models.Model):
     def create(self,values):
         # your logic goes here
         producto = self.env['mta.producto'].browse(self.product_id)
+        if(producto):
+            print('lo encontré')
         oc_actual = producto.oc
         producto.oc = oc_actual + self.product_uom_qty
         override_create = super(StockMove,self).create(values)
