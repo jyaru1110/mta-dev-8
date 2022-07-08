2d744992a972166e5ebc153f5696ed1b17e26149
jyaru1110 <odoo@ampd1.odoo.com>
2022-07-08 23:09:18 +0000

    debugging stocke move create method

--- app_mta/models/.ipynb_checkpoints/product_product-checkpoint.py
+++ app_mta/models/.ipynb_checkpoints/product_product-checkpoint.py
@@ -4,12 +4,17 @@ from odoo import models, fields, api
 class ProductProduct(models.Model):
     _inherit = 'product.product'
     
+    def get_id(self):
+        print(self.id)
+        return self.id
+    
     @api.model
     def create(self,values):
         # your logic goes here
         print("entra aquí")
-        print(values)
-        product_info={'product_tmpl_id':values['id']}
-        self.env['mta.producto'].create(product_info)
+        print(get_id())
+        #print(values)
+        #product_info={'product_tmpl_id':values['id']}
+        #self.env['mta.producto'].create(product_info)
         override_create = super(ProductProduct,self).create(values)
         return override_create
