b0ca51982f6e8a63a4222e30ff9ea35715853b53
jyaru1110 <odoo@ampd1.odoo.com>
2022-07-08 23:11:56 +0000

    debugging stocke move create method

--- app_mta/models/product_product.py
+++ app_mta/models/product_product.py
@@ -4,15 +4,11 @@ from odoo import models, fields, api
 class ProductProduct(models.Model):
     _inherit = 'product.product'
     
-    def get_id(self):
-        print(self.id)
-        return self.id
-    
     @api.model
     def create(self,values):
         # your logic goes here
         print("entra aquí")
-        print(get_id())
+        print(self)
         #print(values)
         #product_info={'product_tmpl_id':values['id']}
         #self.env['mta.producto'].create(product_info)
