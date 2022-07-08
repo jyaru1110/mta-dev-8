e15d2e58ae0d10bec39d17255bbeb24852a2cb50
jyaru1110 <odoo@ampd1.odoo.com>
2022-07-08 23:00:40 +0000

    debugging stocke move create method

--- app_mta/models/.ipynb_checkpoints/stock_move-checkpoint.py
+++ app_mta/models/.ipynb_checkpoints/stock_move-checkpoint.py
@@ -7,7 +7,9 @@ class StockMove(models.Model):
     @api.model
     def create(self,values):
         # your logic goes here
-        producto = self.env['mta.producto'].browse(values['product_id'])
+        producto = self.env['mta.producto'].search([('product_tmpl_id','=',values['product_id'])])
+        print(producto)
+        print(producto.oc)
         oc_actual = producto.oc
         producto.oc = oc_actual + values['product_uom_qty']
         override_create = super(StockMove,self).create(values)
