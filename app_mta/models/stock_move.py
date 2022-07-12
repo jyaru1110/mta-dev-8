e75aac53a20d937c934e33e632909d17547f09da
jyaru1110 <odoo@ampd1.odoo.com>
2022-07-12 17:42:31 +0000

    stock move again with values dict

--- app_mta/models/stock_move.py
+++ app_mta/models/stock_move.py
@@ -6,8 +6,8 @@ class StockMove(models.Model):
     
     @api.model
     def create(self,values):
-        override_create = super(StockMove,self).create(values)
-        producto = self.env['mta.producto'].search([('product_tmpl_id','=',self.product_id)])
+        producto = self.env['mta.producto'].search([('product_tmpl_id','=',values['product_id'])])
         oc_actual = producto.oc
-        producto.oc = oc_actual + self.product_uom_qty
+        producto.oc = oc_actual + values['product_uom_qty']
+        override_create = super(StockMove,self).create(values)
         return override_create
