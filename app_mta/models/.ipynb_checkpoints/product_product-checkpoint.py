5dfe5ac086f99da392501485d8e42ef8f21a8c6e
jyaru1110 <odoo@ampd1.odoo.com>
2022-07-13 07:02:06 +0000

    sintaxis error

--- app_mta/models/.ipynb_checkpoints/product_product-checkpoint.py
+++ app_mta/models/.ipynb_checkpoints/product_product-checkpoint.py
@@ -13,6 +13,6 @@ class ProductProduct(models.Model):
     @api.model
     def create(self,values):
         override_create = super(ProductProduct,self).create(values)
-        product_info={'product_tmpl_id':override_create.id,'be_mta_mon':override_create.be_mta_mon,'dbm_v':override_create.dbm_v,'dbm_r':override_create.dbm_r,'lt':override_create.lt,'loteOptimo':override_create.loteOptimo,'qty_trasit':override_create.qty_transit}
+        product_info={'product_tmpl_id':override_create.id,'be_mta_mon':override_create.be_mta_mon,'dbm_v':override_create.dbm_v,'dbm_r':override_create.dbm_r,'lt':override_create.lt,'loteOptimo':override_create.loteOptimo,'qty_transit':override_create.qty_transit}
         self.env['mta.producto'].create(product_info)
         return override_create
