15fc8e9ca1705a6ac7214e5d7419a17519508884
jyaru1110 <odoo@ampd1.odoo.com>
2022-07-21 00:43:01 +0000

    test ir cron

--- app_mta/__manifest__.py
+++ app_mta/__manifest__.py
@@ -13,7 +13,8 @@
             'security/ir.model.access.csv',
             'views/mta_app_view.xml',
             'views/product_view.xml',
-            'views/product_views_inherit.xml'
+            'views/product_views_inherit.xml',
+            'schedule/schedule.xml'
     ],
     'assets': {
         'web.assets_backend': [
