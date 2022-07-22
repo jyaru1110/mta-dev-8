32f99ac79a58cdb8155e8019e445623d06661c3e
jyaru1110 <odoo@ampd1.odoo.com>
2022-07-22 03:15:34 +0000

    canvas pero no chart

--- app_mta/static/src/js/.ipynb_checkpoints/chart_widget-checkpoint.js
+++ app_mta/static/src/js/.ipynb_checkpoints/chart_widget-checkpoint.js
@@ -12,14 +12,9 @@ var ChartWidget = AbstractField.extend({
     _render: function() {
         var self = this;
         var value = this.value;
-        var canvas = this.$('.canvas')
+        var canvas = this.$('#canvas')[0]
         var ctx = canvas.getContext("2d");
         
-        var grd = ctx.createLinearGradient(0, 0, 200, 0);
-        grd.addColorStop(0, "red");
-        grd.addColorStop(1, "white");
-        ctx.fillStyle = grd;
-        ctx.fillRect(10, 10, 150, 80);
     },
 })
 fieldRegistry.add('chart_widget', ChartWidget);
