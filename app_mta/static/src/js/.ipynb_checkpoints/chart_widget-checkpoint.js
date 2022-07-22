65db1625f44284d6f6dd34db309bcd0622025aae
jyaru1110 <odoo@ampd1.odoo.com>
2022-07-22 02:32:12 +0000

    canvas test

--- app_mta/static/src/js/.ipynb_checkpoints/chart_widget-checkpoint.js
+++ app_mta/static/src/js/.ipynb_checkpoints/chart_widget-checkpoint.js
@@ -12,6 +12,14 @@ var ChartWidget = AbstractField.extend({
     _render: function() {
         var self = this;
         var value = this.value;
+        var canvas = this.$('.canvas')
+        var ctx = canvas.getContext("2d");
+        
+        var grd = ctx.createLinearGradient(0, 0, 200, 0);
+        grd.addColorStop(0, "red");
+        grd.addColorStop(1, "white");
+        ctx.fillStyle = grd;
+        ctx.fillRect(10, 10, 150, 80);
     },
 })
 fieldRegistry.add('chart_widget', ChartWidget);
