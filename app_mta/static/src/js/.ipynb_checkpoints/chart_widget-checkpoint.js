b59f583f4d285f207ca0ca04b255850886d4d41e
jyaru1110 <odoo@ampd1.odoo.com>
2022-07-26 00:44:08 +0000

    chart working

--- app_mta/static/src/js/.ipynb_checkpoints/chart_widget-checkpoint.js
+++ app_mta/static/src/js/.ipynb_checkpoints/chart_widget-checkpoint.js
@@ -12,44 +12,50 @@ var ChartWidget = AbstractField.extend({
     _render: function() {
         var self = this;
         var value = this.value;
-        console.log(value)
         var ajax = require('web.ajax');
         var result = [];
+        var labels = []
+        var data_c =[]
+        var canvas = this.$('.canvas')[0]
+        var ctx = canvas.getContext("2d");
         var enlace = "/get_buffer_changes/"+value.toString();
-        console.log(enlace)
         ajax.jsonRpc(enlace, 'call', {}, {
             'async': false
         }).then(function (data) {
             result.push(data);
-            /*result.foreach(element=>{
-                console.log(element)
-            })*/
-        });
-        console.log(result)
-        var canvas = this.$('.canvas')[0]
-        var ctx = canvas.getContext("2d");
-        const myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Oranges'],
-        datasets: [{
-            fill:true,
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor:'rgba(78,115,223,0.4)',
-            borderColor:'rgba(78, 115, 223, 1)',
-            tension: 0.1
+            result[0].forEach(element=>{
+                //console.log(typeof(element.create_date))
+                //data_c.push({x:element.create_date,y:element.buffer_size})
+                labels.push(element.create_date);
+                data_c.push(element.buffer_size);
+                
+            })
+            const myChart = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: labels,
+                        datasets: [{
+                            fill:true,
+                            label: 'Buffer size',
+                            data: data_c,
+                            backgroundColor:'rgba(78,115,223,0.4)',
+                            borderColor:'rgba(78, 115, 223, 1)',
+                            tension: 0.1
             //borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+                        }]
+                    },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+        });
+        //console.log(labels)
+        //console.log(data_c)
+        
         
     },
 })
