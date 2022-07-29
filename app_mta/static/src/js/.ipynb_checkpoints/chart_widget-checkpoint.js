feab78a539663eb0d57a97bbf301efb353b6bf4d
jyaru1110 <odoo@ampd1.odoo.com>
2022-07-29 00:29:23 +0000

    create two changes time

--- app_mta/static/src/js/.ipynb_checkpoints/chart_widget-checkpoint.js
+++ app_mta/static/src/js/.ipynb_checkpoints/chart_widget-checkpoint.js
@@ -42,15 +42,11 @@ var ChartWidget = AbstractField.extend({
             const hoy = new Date(tiempoTranscurrido);
             //console.log(hoy)
             /*console.log(hoy.toUTCString())*/
-            console.log(data[data.length-1])
             data_v.push({x:hoy,y:data[data.length-1].buffer_size});
             data_a.push({x:hoy,y:2*data[data.length-1].buffer_size/3});
             data_r.push({x:hoy,y:data[data.length-1].buffer_size/3});
             data_q.push({x:hoy,y:data[data.length-1].qty_available});
-            console.log(data_v)
-            console.log(data_a)
-            console.log(data_r)
-            console.log(data_q)
+           
             const myChart = new Chart(ctx, {
                     type: 'line',
                     data: {
