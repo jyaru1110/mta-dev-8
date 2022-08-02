100036d2f310f19cc5c6819a7e2934f4af80b81b
jyaru1110 <odoo@ampd1.odoo.com>
2022-08-02 20:18:51 +0000

    sales dont update mta app

--- app_mta/models/.ipynb_checkpoints/stock_quant-checkpoint.py
+++ app_mta/models/.ipynb_checkpoints/stock_quant-checkpoint.py
@@ -8,11 +8,17 @@ class StockQuant(models.Model):
     @api.model
     def create(self,values):
         override_create = super(StockQuant,self).create(values)
+        if(override_create.location_id.id==14):
+            producto_mta = self.env['mta.producto'].search([('product_tmpl_id','=',override_create.product_id.id)])
         if(override_create.location_id.id==4):
         #self.env['changes.time'].create({'product_tmpl_id':override_create.id})
             producto_mta = self.env['mta.producto'].search([('product_tmpl_id','=',override_create.product_id.id)])
             oc_actual = producto_mta.oc
             producto_mta.oc = oc_actual - override_create.inventory_diff_quantity
+            self.env['changes.time'].create({'product_id':producto_mta.id,'qty_available':producto_mta.qty_available+override_create.inventory_diff_quantity,'buffer_size':producto_mta.buffer_size,'type':'available'})
+            
+        if(override_create.location_id.id==5):
+            producto_mta = self.env['mta.producto'].search([('product_tmpl_id','=',override_create.product_id.id)])
             self.env['changes.time'].create({'product_id':producto_mta.id,'qty_available':producto_mta.qty_available+override_create.inventory_diff_quantity,'buffer_size':producto_mta.buffer_size,'type':'available'})
         return override_create
     
@@ -28,5 +34,8 @@ class StockQuant(models.Model):
             producto_mta = self.env['mta.producto'].search([('product_tmpl_id','=',self._origin.product_id.id)])
             oc_actual = producto_mta.oc
             producto_mta.oc = oc_actual + dif
+            self.env['changes.time'].create({'product_id':producto_mta.id,'qty_available':producto_mta.qty_available-dif,'buffer_size':producto_mta.buffer_size,'type':'available'})
+        if self._origin.location_id.id==5:
+            producto_mta = self.env['mta.producto'].search([('product_tmpl_id','=',self._origin.product_id.id)])
             self.env['changes.time'].create({'product_id':producto_mta.id,'qty_available':producto_mta.qty_available-dif,'buffer_size':producto_mta.buffer_size,'type':'available'})
         return override_write
