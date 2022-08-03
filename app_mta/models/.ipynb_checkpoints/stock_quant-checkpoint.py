a797cbd5cd19678f21146d45989e6fd7a80b38f1
jyaru1110 <odoo@ampd1.odoo.com>
2022-08-03 00:41:29 +0000

    rebuild branch

--- app_mta/models/.ipynb_checkpoints/stock_quant-checkpoint.py
+++ app_mta/models/.ipynb_checkpoints/stock_quant-checkpoint.py
@@ -20,10 +20,7 @@ class StockQuant(models.Model):
             producto_mta.oc = oc_actual - override_create.inventory_diff_quantity
             self.env['changes.time'].create({'product_id':producto_mta.id,'qty_available':producto_mta.qty_available+override_create.inventory_diff_quantity,'buffer_size':producto_mta.buffer_size,'type':'available'})
             
-        if(override_create.location_id.id==5):
-            producto_mta = self.env['mta.producto'].search([('product_tmpl_id','=',override_create.product_id.id)])
-            self.env['changes.time'].create({'product_id':producto_mta.id,'qty_available':producto_mta.qty_available+override_create.inventory_diff_quantity,'buffer_size':producto_mta.buffer_size,'type':'available'})
-        return override_create
+        
     
     def write(self,values):
         old_inventory_quantity_auto_apply = self._origin.inventory_quantity_auto_apply
@@ -38,7 +35,4 @@ class StockQuant(models.Model):
             oc_actual = producto_mta.oc
             producto_mta.oc = oc_actual + dif
             self.env['changes.time'].create({'product_id':producto_mta.id,'qty_available':producto_mta.qty_available-dif,'buffer_size':producto_mta.buffer_size,'type':'available'})
-        if self._origin.location_id.id==5:
-            producto_mta = self.env['mta.producto'].search([('product_tmpl_id','=',self._origin.product_id.id)])
-            self.env['changes.time'].create({'product_id':producto_mta.id,'qty_available':producto_mta.qty_available-dif,'buffer_size':producto_mta.buffer_size,'type':'available'})
-        return override_write
+        
